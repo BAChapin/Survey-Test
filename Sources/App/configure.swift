e22c4fa31cc18a7e34c019f225b9b059de99ffae
Brett Chapin <brett.chapin89@gmail.com>
2020-11-11 11:27:59 -0500

    Initial Configuration
    Added Fluent and MongoDriver to packages list, configured the MongoDB database in the configure.swift file.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,13 @@
 import Vapor
+import Fluent
+import FluentMongoDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    try app.databases.use(.mongo(connectionString: "mongodb://localhost:27017/testdb"), as: .mongo)
 
     // register routes
     try routes(app)
